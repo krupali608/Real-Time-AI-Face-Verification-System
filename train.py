c030dc64b1a1cb8fe864daa78498491104f81a85
krupali608 <148405384+krupali608@users.noreply.github.com>
2025-04-29 15:41:39 -0400

    Delete train.py

--- train.py
+++
@@ -1,9 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO('yolov8n.pt')
-
-def main():
-    model.train(data='Anti-Spoofing/Dataset/splitData/dataOffline.yaml', epochs=50)
-
-if __name__ == '__main__':
-    main()
