15f05e396cf3d1c65fc76e030480099cdc35a6f0
krupali608 <148405384+krupali608@users.noreply.github.com>
2025-04-29 15:30:47 -0400

    train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO('yolov8n.pt')
 
 def main():
-    model.train(data='/Users/krupalishinde/Anti-Spoofing/Dataset/splitData/dataOffline.yaml', epochs=50)
+    model.train(data='Anti-Spoofing/Dataset/splitData/dataOffline.yaml', epochs=50)
 
 if __name__ == '__main__':
     main()
